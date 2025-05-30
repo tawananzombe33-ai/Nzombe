8a58e599a35e575cda28294bca580070ee52bd23
B.M.B-TECH <tzsailas@gmail.com>
2025-05-30 22:50:00 +0300

    Update menu.js

--- commander/menu.js
+++ commander/menu.js
@@ -1,95 +1,101 @@
-const util = require('util');
-const fs = require('fs-extra');
-const { zokou } = require(__dirname + "/../framework/zokou");
-const { format } = require(__dirname + "/../framework/mesfonctions");
-const os = require("os");
+const {
+  zokou
+} = require(__dirname + "/../framework/zokou");
+const {
+  format
+} = require(__dirname + "/../framework/mesfonctions");
+const os = require('os');
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
-const more = String.fromCharCode(8206)
-const readmore = more.repeat(4001)
-
-zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-    let { cm } = require(__dirname + "/../framework//zokou");
-    var coms = {};
-    var mode = "public";
-    
-    if ((s.MODE).toLocaleLowerCase() != "yes") {
-        mode = "private";
+zokou({
+  'nomCom': "menu",
+  'categorie': "Menu"
+}, async (_0x160fc2, _0x219205, _0xe5791b) => {
+  let {
+    ms: _0xb19de,
+    repondre: _0x41afad,
+    prefixe: _0x14d418,
+    nomAuteurMessage: _0x11cbde,
+    mybotpic: _0xd7082a
+  } = _0xe5791b;
+  let {
+    cm: _0x256458
+  } = require(__dirname + "/../framework/zokou");
+  let _0x199ed8 = {};
+  let _0x15c89f = "public";
+  if (s.MODE.toLowerCase() !== "yes") {
+    _0x15c89f = "private";
+  }
+  _0x256458.map(_0x3a2d5b => {
+    if (!_0x199ed8[_0x3a2d5b.categorie]) {
+      _0x199ed8[_0x3a2d5b.categorie] = [];
     }
-
-
-    
- cm.map(async (com, index) => {
-        if (!coms[com.categorie])
-            coms[com.categorie] = [];
-        coms[com.categorie].push(com.nomCom);
-    });
-
-    moment.tz.setDefault('EAT');
-
-// Créer une date et une heure en EAT
-const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD/MM/YYYY');
-
-  let infoMsg =  `
-╭──────────────────❂
-┊❂╭───*Mr b² 🍁🍁*────❂
-┊✺┊ *User* : ${s.OWNER_NAME}
-┊✺┊ *Mode* : ${mode}
-┊✺╰───────────────❂
-┊✺┊ *Time* : ${temps}  
-┊✺┊ *Ram* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
-┊❂╰───────────────❂
-╰──────────────────❂ \n\n`;
- 
-    let menuMsg=`  
-  **Mr b² 🍁🍁 COMMANDS*
-`;
-
-    for (const cat in coms) {
-        menuMsg += `*╭────❂* *${cat}* *❂*`;
-        for (const cmd of coms[cat]) {
-            menuMsg += `  
-*┊❂* ${cmd}`;
+    _0x199ed8[_0x3a2d5b.categorie].push(_0x3a2d5b.nomCom);
+  });
+  moment.tz.setDefault("Africa/Dar Es Salam");
+  const _0x4c271f = moment().hour();
+  let _0x57980c = "🌆Good morning";
+  if (_0x4c271f >= 12 && _0x4c271f < 18) {
+    _0x57980c = "🌆Good Afternoon";
+  } else {
+    if (_0x4c271f >= 18) {
+      _0x57980c = "🌆Good everything";
+    } else if (_0x4c271f >= 22 || _0x4c271f < 5) {
+      _0x57980c = "🌆Good night";
+    }
+  }
+  ;
+  moment.tz.setDefault("Etc/GMT");
+  const _0x483331 = moment().format("DD/MM/YYYY");
+  let _0x2b004d = "\n *Hello👏:* *" + _0x57980c + "*\n\n╭┈┈┈┈┈┈┈┈┈┈┈┈⊷\n┊▸ *ʙᴏᴛ ᴏᴡɴᴇʀ : Mr b² 🍁🍁*\n┊▸ *ᴘʀᴇғɪx :* [ " + s.PREFIXE + " ]\n┊▸ *ᴍᴏᴅᴇ :* " + _0x15c89f + "\n┊▸ *ᴅᴀᴛᴇ :* " + _0x483331 + "\n┊▸ *ᴘʟᴀᴛғᴏʀᴍ :* " + os.platform() + "\n┊▸ *ᴄᴏᴍᴍᴀɴᴅꜱ :* " + _0x256458.length + "\n┊▸ *ʀᴀᴍ :* " + format(os.totalmem() - os.freemem()) + '/' + format(os.totalmem()) + "\n╰┈┈┈┈┈┈┈┈┈┈┈⊷\n\n";
+  let _0x3eb70a = "*𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒*";
+  for (const _0x1a3082 in _0x199ed8) {
+    _0x3eb70a += "\n*「 " + _0x1a3082 + " 」*\n╭┈┈┈┈┈┈┈┈┈┈┈⊷ ";
+    for (const _0x410fe3 of _0x199ed8[_0x1a3082]) {
+      _0x3eb70a += " \n┊▸ " + _0x410fe3;
+    }
+    _0x3eb70a += "\n╰┈┈┈┈┈┈┈┈┈┈┈⊷";
+  }
+  _0x3eb70a += "\n> Made By boy king Mr b² 🍁🍁\n";
+  try {
+    await _0x219205.sendMessage(_0x160fc2, {
+      'text': _0x2b004d + _0x3eb70a,
+      'contextInfo': {
+        'externalAdReply': {
+          'title': "Mr b² 🍁🍁",
+          'body': "king boy rah",
+          'thumbnailUrl': "https://files.catbox.moe/z6jrqz.jpg",
+          'sourceUrl': "https://whatsapp.com/channel/0029VawO6hgF6sn7k3SuVU3z",
+          'mediaType': 0x1,
+          'renderLargerThumbnail': true
         }
-        menuMsg += `
-*╰═════════════❂* \n`
-    }
-
-    menuMsg += `
-◇            ◇
-*—————✺✺✺✺—————*
-
-  *Mr b² 🍁🍁*                                         
-*╰═════════════❂*
-`;
-
-   var lien = mybotpic();
-
-   if (lien.match(/\.(mp4|gif)$/i)) {
-    try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" , gifPlayback : true }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-// Vérification pour .jpeg ou .png
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-    try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "*bmb-tech*" }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-else {
-    
-    repondre(infoMsg + menuMsg);
-    
-}
-
+      }
+    });
+    await _0x219205.sendMessage(_0x160fc2, {
+      'audio': {
+        'url': "https://files.catbox.moe/raje26.mp3"
+      },
+      'mimetype': "audio/mp4",
+      'ptt': true,
+      'caption': "Mr b² 🍁🍁 SONG",
+      'contextInfo': {
+        'isForwarded': true,
+        'forwardedNewsletterMessageInfo': {
+          'newsletterJid': "120363382023564830@newsletter",
+          'newsletterName': "Mr b² 🍁🍁",
+          'serverMessageId': -1
+        },
+        'forwardingScore': 0x3e7,
+        'externalAdReply': {
+          'body': "Mr b² 🍁🍁",
+          'thumbnailUrl': "https://files.catbox.moe/z6jrqz.jpg",
+          'sourceUrl': "https://whatsapp.com/channel/0029VawO6hgF6sn7k3SuVU3z",
+          'rendersmallThumbnail': false
+        }
+      }
+    });
+  } catch (_0x4819a5) {
+    console.log("Error fetching data:", error);
+    _0x41afad("❌ Error fetching repository data. Please try again later.");
+  }
 });
