36cc0fe3e5d3500cddaf126c21dfa99fbf73d819
side-smile <ibrahimsaiid066@gmail.com>
2025-08-05 15:37:10 +0300

    Update stickersearch (1).js

--- commander/stickersearch (1).js
+++ commander/stickersearch (1).js
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'B.M.B-TECH',
+      author: '𝛭𝑅 𝐵² MD',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
